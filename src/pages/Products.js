9048dac8cd4bfe1c9b5570a1192b252d6d44e447
dineshkumar24v <dineshkumar24v@gmail.com>
2025-10-20 20:57:58 +0530

    updated

--- src/pages/Products.js
+++ src/pages/Products.js
@@ -126,7 +126,7 @@ const Products = () => {
                 className="w-full px-3 py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-primary-500"
               >
                 <option value="">All Categories</option>
-                {categories.map((cat) => (
+                {(categories || []).map((cat) => (
                   <option key={cat} value={cat}>
                     {cat}
                   </option>
