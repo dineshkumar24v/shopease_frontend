61330558a8affedb056fe0f11a8247e450d6c40a
dineshkumar24v <dineshkumar24v@gmail.com>
2025-10-20 20:51:44 +0530

    updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -14,7 +14,7 @@ const Home = () => {
   const fetchFeaturedProducts = async () => {
     try {
       const response = await axios.get("/api/products?featured=true&limit=6");
-      setFeaturedProducts(response.data.products);
+      setFeaturedProducts(response?.data?.products);
     } catch (error) {
       console.error("Error fetching products:", error);
     } finally {
